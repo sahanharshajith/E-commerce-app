729067cb8e1c784b97a0f5887cc0dbef00b93d86
sahanharshajith <sahan1234harshajith@gmail.com>
2025-04-23 14:11:22 +0530

    feat: Add initial frontend structure with navigation and pages
    - Implemented NavBar component with responsive design and dropdown menu
    - Created placeholder pages for Home, About, Collection, Contact, Cart, Login, Orders, PlaceOrder, and Product
    - Added CSS for styling and imported Tailwind CSS
    - Integrated routing using React Router
    - Included various asset images such as logos and icons

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Cart from './pages/Cart'
+import Product from './pages/Product'
+import NavBar from './components/NavBar'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection/>} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/product/:productId' element={<Product />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App

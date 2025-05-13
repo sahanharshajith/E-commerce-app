1e9fb6856653897d21292ffeb19018ec863be9d9
sahanharshajith <sahan1234harshajith@gmail.com>
2025-05-13 19:24:43 +0530

    feat: Add BestSeller component and integrate it into Home page

---
+++ frontend/src/components/BestSeller.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useContext, useEffect, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+const BestSeller = () => {
+
+    const { products } = useContext(ShopContext);
+    const [bestSeller, setBestSeller] = useState([]);
+
+    useEffect(()=>{
+        const bestSellerProducts = products.filter((item) => item.bestseller);
+        setBestSeller(bestSellerProducts.slice(0, 5));
+    }, [products]);
+
+  return (
+        <div className='my-10'>
+            <div className='text-center py-8 text-3xl'>
+                <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
+                <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                </p>
+            </div>
+
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>   
+                {bestSeller.map((item) => (
+                    <ProductItem 
+                        key={item._id} // Better to use item._id than index
+                        id={item._id} 
+                        image={item.image} 
+                        name={item.name} 
+                        price={item.price} 
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default BestSeller
